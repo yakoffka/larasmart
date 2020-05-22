f2c917441b30ba0411161a677c2a95aa6697af30
yakoffka <yagithub@mail.ru>
2020-05-22 03:17:01 +0300

    Add default user     php artisan make:seeder UserSeeder

---
+++ config/custom.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'user' => [
+        'name' => env('USER_NAME', 'admin'),
+        'email' => env('USER_EMAIL', 'example@site.com'),
+        'password' => env('USER_PASSWORD', '11111111'),
+    ],
+];
---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => config('custom.user.name'),
+            'email' => config('custom.user.name'),
+            'password' => Hash::make(config('custom.user.password')),
+        ]);
+    }
+}
